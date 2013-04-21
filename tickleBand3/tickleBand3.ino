e180a2ca3900a720f3355ff5e00d94d7d6750d9f
Kevin Ho <keho98@gmail.com>
2013-04-20 23:46:22 -0700

    Verified version of standalone tickleBand

--- tickleBand3/tickleBand3.ino
+++ tickleBand3/tickleBand3.ino
@@ -3,25 +3,74 @@
 #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
 #define TIME_HEADER  'T'   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+#define NUM_TRIGGERS 4
+
+// set pin numbers
+const int buttonPin = 2;
+
+int buttonState = 0;
+int lastButtonState = LOW;
+int numberPresses = 0;
+
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 // T1262347200  //noon Jan 1 2010
 // Trigger hours
-int[] TriggerHours = {9,12,18,24}
+int TriggerHours[] = {9,12,18,24};
 
 void setup()  {
   Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
 }
 
-void loop(){    
+void loop(){  
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button 
+  // (i.e. the input went from LOW to HIGH),  and you've waited 
+  // long enough since the last press to ignore any noise:  
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  } 
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+    buttonState = reading;
+  }
+  
+  if(buttonState == HIGH){
+    numberPresses++;
+  }
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
   if(Serial.available() ) 
   {
     processSyncMessage();
   }
   if(timeStatus() == timeNotSet) 
     Serial.println("waiting for sync message");
-  else     
-      digitalClockDisplay();  
-  delay(100);
+  else{
+    boolean trigger = false;
+    for(int i = 0; i< NUM_TRIGGERS; i++){
+      if(hour() == TriggerHours[i]){
+        trigger = true; 
+      }
+    }
+    if(trigger){
+      triggerTickle(); 
+    }
+  }
+    delay(100);
+}
+
+void triggerTickle(){
+  
 }
 
 void digitalClockDisplay(){
