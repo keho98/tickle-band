5b6746bb4ba30ae235d9ec667e6b30ee57373905
Kevin Ho <keho98@gmail.com>
2013-04-25 21:19:52 -0700

    Working on wired version

--- tickleBand2/tickleBand2.ino
+++ tickleBand2/tickleBand2.ino
@@ -1,6 +1,9 @@
-#define IO_PAIRS 3
-int analogOutputPins[] = {3,5,6};
-int analogInputPins[] = {0,1,2};
+#include <Servo.h> 
+#define DEBUG true
+
+const int buttonPins[] = {A3, A1};
+const int numInputs = 2;
+
 char incomingByte = 0;
 boolean DEBUG = false;
 
@@ -9,15 +12,36 @@ int led = 13;
 void setup() 
 {
   Serial.begin(9600);
+  for(int i=0; i<numInputs; i++){
+     pinMode(buttonPins[i], INPUT); 
+     digitalWrite(buttonPins[i], HIGH); 
+     lastButtonState[i] = LOW;
+  }
   pinMode(led,OUTPUT);
 }
 
 void loop() 
 {
+  int reading[numInputs];
+  for(int i = 0; i < numInputs; i++){
+    reading[i] = digitalRead(buttonPins[i]); 
+    // If the switch changed, due to noise or pressing:
+    if (reading[i] != lastButtonState[i]) {
+      // reset the debouncing timer
+      if(reading[i] == HIGH){
+        numberPresses++; 
+        sendTickle();
+      }
+    } 
+    // save the reading.  Next time through the loop,
+    // it'll be the lastButtonState:
+    lastButtonState[i] = reading[i];
+  }
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
     // say what you got:
+    triggerTickle()
     digitalWrite(led,HIGH);
   }
   else{
@@ -25,4 +49,12 @@ void loop()
   }
   // Wait 100 milliseconds
   delay(200);
+}
+
+void triggerTickle(){
+  
+}
+
+void sendTickle(){
+  Serial.write('t');
 }
