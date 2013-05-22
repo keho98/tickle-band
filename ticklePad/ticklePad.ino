8c5317b506503bf149184e7bf6661559fbea1be0
Kevin Ho <keho98@gmail.com>
2013-05-22 16:04:52 -0700

    Captouch + ServoXY Working

--- ticklePad/ticklePad.ino
+++ ticklePad/ticklePad.ino
@@ -13,9 +13,23 @@
  
 #include <Wire.h>
 #include <mpr121.h>
- 
+#include <Servo.h> 
+
+#define MAX_X 9
+#define MAX_Y 13
+#define MIN_X 1
+#define MIN_Y 1
+
+#define MICRO_MAX 2200
+#define MICRO_MIN 800
+
 int X ;           // X-coordinate
 int Y ;           // Y-coordinate
+
+Servo servoX;
+Servo servoY;
+const int servoPinX = 8;
+const int servoPinY = 6;
  
 // =========  setup  =========
 void setup()
@@ -24,22 +38,38 @@ void setup()
   Serial.begin(19200);
   Wire.begin();
   CapaTouch.begin();
- 
+  //Initialize servos
+  servoX.attach(servoPinX);
+  servoY.attach(servoPinY);
   delay(500);
   Serial.println("START"); 
 }
- 
+
+void setX(int x){
+  //Scale x
+  float microSeconds = float(MICRO_MAX - MICRO_MIN) /float(MAX_X - MIN_X) * x + MICRO_MIN;
+  servoX.writeMicroseconds(int(microSeconds));
+  Serial.println(int(microSeconds));
+}
+
+void setY(int y){
+ float microSeconds = float(MICRO_MAX - MICRO_MIN) / float(MAX_Y - MIN_Y) * y + MICRO_MIN;
+ servoY.writeMicroseconds(int(microSeconds)); 
+ Serial.println(int(microSeconds));
+}
  
 // =========  loop  =========
 void loop()
 {   
   X=CapaTouch.getX();              // Get X position.
   Y=CapaTouch.getY();              // Get Y position.
-  if(X>=1&&X<=9&&Y>=1&&Y<=13)
+  if(X>=MIN_X&&X<=MAX_X&&Y>=MIN_Y&&Y<=MAX_Y)
    {                                 // Determine whether in the range.If not,do nothing.
       Serial.print(X);
       Serial.print(",");
       Serial.println(Y); 
+      setX(X);
+      setY(Y);
    }
   delay(200);
 }
